ae46f39e4b39745e8c5c0e34cf6260cfe80b5c13
Raul Cardoso <byruzie@gmail.com>
2025-06-03 00:43:35 -0300

    creating a basic test route

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,21 @@ const router = require("./routes/index");
 const connection = require("./db/connection");
 const tables = require("./db/tables");
 
+// Middlewares
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Banco de dados e rotas
 tables.init(connection);
 router(app);
 
+// Rota raiz (teste no Railway)
+app.get("/", (req, res) => {
+  res.send("Servidor está rodando! 🚀");
+});
+
+// Inicializa o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
